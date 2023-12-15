4c46b6c4bae15c5d2a9460ab5dfbc1f6d1c0cb90
Abhishek P <113097985+abhiiishek-10@users.noreply.github.com>
2023-12-15 18:57:46 +0530

    fix: lenis fixed

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// lenis start
+
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1 - 79
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+// Edge (based on chromium) detection
+var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 
-
-// gsap.registerPlugin(ScrollTrigger);
-
-
-// // lenis start
-// const lenis = new Lenis()
-// function raf(time) {
-//     lenis.raf(time)
-//     requestAnimationFrame(raf)
-// }
-// requestAnimationFrame(raf)
+var output = 'Detecting browsers by ducktyping:<hr>';
+output += 'isFirefox: ' + isFirefox + '<br>';
+output += 'isChrome: ' + isChrome + '<br>';
+output += 'isSafari: ' + isSafari + '<br>';
+output += 'isOpera: ' + isOpera + '<br>';
+output += 'isIE: ' + isIE + '<br>';
+output += 'isEdge: ' + isEdge + '<br>';
+output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
+output += 'isBlink: ' + isBlink + '<br>';
 
 
+let lenis;
+if (!ScrollTrigger.isTouch) {
+    lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+    function raf(time) {
+        lenis.raf(time);
+        ScrollTrigger.update();
+        requestAnimationFrame(raf);
+    }
 
+    requestAnimationFrame(raf);
 
+    if (isFirefox == false) {
+        ScrollTrigger.normalizeScroll(true);
+    }
 
+}
 // lenis end
-
-
 
 
 
@@ -30,66 +69,6 @@
 // Pre loader
 $(document).ready(function () {
 
-    gsap.registerPlugin(ScrollTrigger);
-
-    // lenis start
-
-    // Opera 8.0+
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-
-    // Safari 3.0+ "[object HTMLElementConstructor]" 
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-
-    // Internet Explorer 6-11
-    var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-    // Edge 20+
-    var isEdge = !isIE && !!window.StyleMedia;
-
-    // Chrome 1 - 79
-    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-    // Edge (based on chromium) detection
-    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
-
-    // Blink engine detection
-    var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-
-    var output = 'Detecting browsers by ducktyping:<hr>';
-    output += 'isFirefox: ' + isFirefox + '<br>';
-    output += 'isChrome: ' + isChrome + '<br>';
-    output += 'isSafari: ' + isSafari + '<br>';
-    output += 'isOpera: ' + isOpera + '<br>';
-    output += 'isIE: ' + isIE + '<br>';
-    output += 'isEdge: ' + isEdge + '<br>';
-    output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
-    output += 'isBlink: ' + isBlink + '<br>';
-
-
-    let lenis;
-    if (!ScrollTrigger.isTouch) {
-        lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-        });
-        function raf(time) {
-            lenis.raf(time);
-            ScrollTrigger.update();
-            requestAnimationFrame(raf);
-        }
-
-        requestAnimationFrame(raf);
-
-        if (isFirefox == false) {
-            ScrollTrigger.normalizeScroll(true);
-        }
-
-    }
-    // lenis end
 
     preLoader();
 
