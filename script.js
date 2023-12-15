5db244ed4dd4a32ec85acbcbee5992e26cebb74f
Abhishek P <113097985+abhiiishek-10@users.noreply.github.com>
2023-12-15 17:30:56 +0530

    Fix: Lenis stopped on touch devices

--- script.js
+++ script.js
@@ -14,65 +14,6 @@
 
 
 
-gsap.registerPlugin(ScrollTrigger);
-
-
-
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-
-// Chrome 1 - 79
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-// Edge (based on chromium) detection
-var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
-
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-
-var output = 'Detecting browsers by ducktyping:<hr>';
-output += 'isFirefox: ' + isFirefox + '<br>';
-output += 'isChrome: ' + isChrome + '<br>';
-output += 'isSafari: ' + isSafari + '<br>';
-output += 'isOpera: ' + isOpera + '<br>';
-output += 'isIE: ' + isIE + '<br>';
-output += 'isEdge: ' + isEdge + '<br>';
-output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
-output += 'isBlink: ' + isBlink + '<br>';
-
-
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-});
-
-function raf(time) {
-    lenis.raf(time);
-    ScrollTrigger.update();
-    requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-
-if (isFirefox == false) {
-    ScrollTrigger.normalizeScroll(true);
-}
-
-
-
 
 
 // lenis end
@@ -82,40 +23,73 @@ if (isFirefox == false) {
 
 
 
-// Hacker text hover
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const elements = [...document.querySelectorAll("a.h-effect")]
-elements.map(element => {
 
-    element.onmouseover = event => {
-        let iterations = 0;
-        const interval = setInterval(() => {
-            event.target.innerText = event.target.innerText.split("")
-                .map((letter, index) => {
-
-                    if (index < iterations) {
-                        return event.target.dataset.value[index];
-                    }
-
-                    return letters[Math.floor(Math.random() * 26)]
-                })
-                .join("");
-
-            if (iterations >= event.target.dataset.value.length) {
-                clearInterval(interval);
-            }
-
-
-            iterations += 1 / 3;
-        }, 30);
-
-    }
-})
 
 
 
 // Pre loader
 $(document).ready(function () {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    // lenis start
+
+    // Opera 8.0+
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+
+    // Chrome 1 - 79
+    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+    // Edge (based on chromium) detection
+    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+    // Blink engine detection
+    var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+    var output = 'Detecting browsers by ducktyping:<hr>';
+    output += 'isFirefox: ' + isFirefox + '<br>';
+    output += 'isChrome: ' + isChrome + '<br>';
+    output += 'isSafari: ' + isSafari + '<br>';
+    output += 'isOpera: ' + isOpera + '<br>';
+    output += 'isIE: ' + isIE + '<br>';
+    output += 'isEdge: ' + isEdge + '<br>';
+    output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
+    output += 'isBlink: ' + isBlink + '<br>';
+
+
+    let lenis;
+    if (!ScrollTrigger.isTouch) {
+        lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+        });
+        function raf(time) {
+            lenis.raf(time);
+            ScrollTrigger.update();
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        if (isFirefox == false) {
+            ScrollTrigger.normalizeScroll(true);
+        }
+
+    }
+    // lenis end
 
     preLoader();
 
@@ -166,16 +140,18 @@ $(document).ready(function () {
         navBtn.onclick = function (e) {
             navBtn.classList.toggle('active');
             if (navBtn.classList.contains('active')) {
-                lenis.stop();
+                if (!ScrollTrigger.isTouch) {
+                    lenis.stop();
+                }
             } else {
-                lenis.start();
+                if (!ScrollTrigger.isTouch) {
+                    lenis.start();
+                }
             }
             headerTl.reversed(!headerTl.reversed())
         }
     }
     openNav();
-
-
 
     function preLoader() {
 
@@ -216,9 +192,7 @@ $(document).ready(function () {
                 },
                 "-=2"
             );
-        // .to(".preloader", {
-        //     display: "none"
-        // });
+
         if ($('body').hasClass('home')) {
             document.getElementById('showreel-vid').play();
         }
@@ -228,6 +202,40 @@ $(document).ready(function () {
             $(".preloader").css("display", "none");
         };
     }
+
+
+
+    // Hacker text hover
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const elements = [...document.querySelectorAll("a.h-effect")]
+    elements.map(element => {
+
+        element.onmouseover = event => {
+            let iterations = 0;
+            const interval = setInterval(() => {
+                event.target.innerText = event.target.innerText.split("")
+                    .map((letter, index) => {
+
+                        if (index < iterations) {
+                            return event.target.dataset.value[index];
+                        }
+
+                        return letters[Math.floor(Math.random() * 26)]
+                    })
+                    .join("");
+
+                if (iterations >= event.target.dataset.value.length) {
+                    clearInterval(interval);
+                }
+
+
+                iterations += 1 / 3;
+            }, 30);
+
+        }
+    })
+
+
 })
 
 
